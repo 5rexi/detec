ff4b70ac834d2a88b1fc5722197473b8480ea734
5rexi <woshizhizhang233@163.com>
2026-03-02 14:34:20 +0800

    first commit

--- annotate_vest.py
+++ annotate_vest.py
@@ -3,7 +3,7 @@ from ppe.annotation import annotate_video
 if __name__ == "__main__":
     annotate_video(
         task_name="vest",
-        video_path="./origin_data/wF0Z6eJHVtOkFzh7kGoo000000_1766383200.mp4",
+        video_path="./origin_data/video(11).mp4",
         yolo_weights="weights/yolo11s.pt",
-        save_every_n_frames=3,
+        save_every_n_frames=20,
     )
