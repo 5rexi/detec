e3147df09ddc224d37d994820b7ec4fe0da81f4f
5rexi <woshizhizhang233@163.com>
2026-03-03 10:42:34 +0800

    first commit

--- detect_helmet.py
+++ detect_helmet.py
@@ -3,12 +3,12 @@ from ppe.inference import run_video
 if __name__ == "__main__":
     run_video(
         task_name="helmet",
-        video_path="./origin_data/HZIdNgLggCAUJZqRhrW2000000_1766367658.mp4",
+        video_path="./origin_data/video(23).mp4",
         yolo_weights="weights/yolo11n.pt",
         cls_weights="weights/resnet_helmet.pth",
         output_path="output_helmet.mp4",
-        violation_threshold=0.92,
+        violation_threshold=0.95,
         trigger_score=1.0,
-        min_violation_streak=5,
+        min_violation_streak=7,
         conf=0.5,
     )
--- train_helmet.py
+++ train_helmet.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         batch_size=32,
         lr=1e-4,
         weight_decay=5e-4,
-        class_weights=[1.0, 4.0, 1.0],
+        class_weights=[1.0, 2.0, 1.0],
         false_violation_penalty=3.0,
         dataset_root="dataset/helmet",
         save_path="weights/resnet_helmet.pth",
